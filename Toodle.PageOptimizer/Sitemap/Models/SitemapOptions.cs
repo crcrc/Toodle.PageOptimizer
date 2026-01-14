f31c70c510632535554ec201e6d9b8d71c2fefa9
mattoodle <matt@c16.co.uk>
2026-01-14 09:27:02 +0000

    Adding Sitemap (#1)
    * initial sitemap
    
    * Sitemap - force UTF8 encoding

---
+++ Toodle.PageOptimizer/Sitemap/Models/SitemapOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toodle.PageOptimizer.Sitemap.Models
+{
+    public class SitemapOptions
+    {
+        /// <summary>
+        /// The duration for which the generated sitemap XML is cached.
+        /// Default is 6 hours.
+        /// </summary>
+        public TimeSpan CacheExpiration { get; set; } = TimeSpan.FromHours(6);
+    }
+}
---
+++ Toodle.PageOptimizer/Sitemap/Services/ISitemapSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toodle.PageOptimizer.Sitemap.Models;
+
+namespace Toodle.PageOptimizer.Sitemap.Services
+{
+    public interface ISitemapSource
+    {
+        /// <summary>
+        /// Asynchronously retrieves a collection of SitemapUrl objects.
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation, containing an
+        /// enumerable collection of SitemapUrl objects.
+        /// </returns>
+        Task<IEnumerable<SitemapUrl>> GetUrlsAsync();
+    }
+}
